d4f10ca29c242a878db2e2207fb000fd67395aeb
Adriana Gaglio <adriana.gaglio@gmail.com>
2025-01-11 11:03:10 +0100

    enable email endpoint

---
+++ be/src/main/java/epicode/it/businesstrips/utils/upload/UploadController.java
@@ -0,0 +1,21 @@
+package epicode.it.businesstrips.utils.upload;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/upload")
+@RequiredArgsConstructor
+public class UploadController {
+    private final UploadSvc uploadSvc;
+
+    @PostMapping(consumes = "multipart/form-data")
+    public ResponseEntity<String> uploadFile(MultipartFile file) throws Exception {
+        return new ResponseEntity<>(uploadSvc.uploadFile(file), HttpStatus.CREATED);
+    }
+}
